cf39e3367bbb17cae0a607029d2094c9b81bcabc
LRlaxmirathod <168898815+LRlaxmirathod@users.noreply.github.com>
2024-07-29 13:45:37 +0530

    synchronous and asynchronous

---
+++ index.js
@@ -0,0 +1,18 @@
+// synchronous 
+console.log("laxmi")
+console.log("kartik sir")
+console.log("darshan")
+console.log("indira")
+
+
+// asynchrounous
+
+console.log("start")
+
+setTimeout(() => {
+    console.log("I am settimeout function")
+}, 2000);
+
+console.log("end")
+
+
